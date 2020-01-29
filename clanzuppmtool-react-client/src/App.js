76be021c303dadada9a2ccd78f3ba382c6cbc977
sadig2 <clanzu2@yahoo.com>
2020-01-29 09:07:11 -0500

    htmls in react components

--- clanzuppmtool-react-client/src/App.js
+++ clanzuppmtool-react-client/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import "./components/Dashboard";
 import Dashboard from "./components/Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/layout/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Dashboard />
       </div>
     );
