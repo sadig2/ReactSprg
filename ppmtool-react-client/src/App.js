cc6856eb4f090960bd9171b830b8fa59bfb9726b
sadig2 <clanzu2@yahoo.com>
2020-01-31 09:54:28 -0500

    31jan

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -6,17 +6,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layout/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/dashBoard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashBoard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -2,50 +2,59 @@ import React, { Component } from "react";
 import ProjectItem from "./project/ProjectItem";
 import Header from "./layout/Header";
 import CreateProjectButton from "./project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
     return (
-      <div class="projects">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <h1 class="display-4 text-center">Projects</h1>
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
               <br />
               <CreateProjectButton />
               <br />
               <hr />
 
-              <div class="container">
-                <div class="card card-body bg-light mb-3">
-                  <div class="row">
-                    <div class="col-2">
-                      <span class="mx-auto">REACT</span>
+              <div className="container">
+                <div className="card card-body bg-light mb-3">
+                  <div className="row">
+                    <div className="col-2">
+                      <span className="mx-auto">REACT</span>
                     </div>
-                    <div class="col-lg-6 col-md-4 col-8">
+                    <div className="col-lg-6 col-md-4 col-8">
                       <h3>Spring / React Project</h3>
                       <p>
                         Project to create a Kanban Board with Spring Boot and
                         React
                       </p>
                     </div>
-                    <div class="col-md-4 d-none d-lg-block">
-                      <ul class="list-group">
+                    <div className="col-md-4 d-none d-lg-block">
+                      <ul className="list-group">
                         <a href="#">
-                          <li class="list-group-item board">
-                            <i class="fa fa-flag-checkered pr-1">
+                          <li className="list-group-item board">
+                            <i className="fa fa-flag-checkered pr-1">
                               Project Board{" "}
                             </i>
                           </li>
                         </a>
                         <a href="#">
-                          <li class="list-group-item update">
-                            <i class="fa fa-edit pr-1">Update Project Info</i>
+                          <li className="list-group-item update">
+                            <i className="fa fa-edit pr-1">
+                              Update Project Info
+                            </i>
                           </li>
                         </a>
                         <a href="">
-                          <li class="list-group-item delete">
-                            <i class="fa fa-minus-circle pr-1">
+                          <li className="list-group-item delete">
+                            <i className="fa fa-minus-circle pr-1">
                               Delete Project
                             </i>
                           </li>
@@ -63,4 +72,13 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project
+});
+
+export default connect(mapStateToProps, { getProjects })(Dashboard);
